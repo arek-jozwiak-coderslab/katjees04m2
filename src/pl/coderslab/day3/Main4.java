7990b815e6aac999af097512a982bdea9d344b38
arcyro <arek@arcyro.pl>
2018-10-10 15:46:22 +0200

    day2

--- src/pl/coderslab/day3/Main4.java
+++ src/pl/coderslab/day3/Main4.java
@@ -7,25 +7,29 @@ public class Main4 {
 
 
     public static void main(String[] args) {
-        String QUERY4 = "SELECT * from products;";
+        String QUERY4 = "SELECT avg(rating) as rat from movies;";
 
+        String QUERY5 = "SELECT * FROM movies where rating > ?";
+        double rat = 0;
         try (Connection conn = DriverManager.getConnection(
-                "jdbc:mysql://localhost:3366/products_ex?useSSL=false&characterEncoding=utf8",
+                "jdbc:mysql://localhost:3366/cinemas_ex?useSSL=false&characterEncoding=utf8",
                 "root", "root");
              PreparedStatement stat = conn.prepareStatement(QUERY4);
+             PreparedStatement stat2 = conn.prepareStatement(QUERY5);
              ResultSet rs = stat.executeQuery()) {
 
-            while (rs.next()){
-                int id = rs.getInt("id");
-                String name = rs.getString("name");
-                String description = rs.getString("description");
-                if(description.length()>20){
-                    description = description.substring(0, 19) +"...";
-                }
-                double price = rs.getDouble("price");
-                System.out.println(id +" - " + name + " - " + " - " + description +" - "+ price);
+            while (rs.next()) {
+                rat = rs.getDouble("rat");
             }
 
+            stat2.setDouble(1, rat);
+
+            try( ResultSet rs2 = stat2.executeQuery()){
+                while (rs2.next()) {
+                    System.out.println(rs2.getString("title"));
+                }
+
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         }
